1f63b6a33022afb4739440edc4e0ed46514fe6bf
Abdulla <abdulla.binissa.176@gmail.com>
2021-01-13 13:45:55 -0800

    Handle Navigation
    https://reactnavigation.org/docs/bottom-tab-navigator/
    Started Using Camera/Gallery

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
 import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
 
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
@@ -75,7 +77,8 @@ export class App extends Component {
 				<NavigationContainer>
 					<Stack.Navigator initialRouteName="Landing">
 						<Stack.Screen name="Landing" component={ LandingScreen } options={{ headerShown:false }}/>	
-						<Stack.Screen name="Register" component={ RegisterScreen }/>	
+						<Stack.Screen name="Register" component={ RegisterScreen }/>
+						<Stack.Screen name="Login" component={ LoginScreen }/>	
 					</Stack.Navigator>
 				</NavigationContainer>
 				)
@@ -83,7 +86,14 @@ export class App extends Component {
 
 		return(
 			<Provider store={ store }>
-				<MainScreen/>
+				<NavigationContainer>
+
+					<Stack.Navigator initialRouteName="Main">
+						<Stack.Screen name="Main" component={ MainScreen } options={{ headerShown:false }}/>
+						<Stack.Screen name="Add" component={ AddScreen }/>
+					</Stack.Navigator>
+
+				</NavigationContainer>
 			</Provider>
 		)
 	}
--- components/Main.js
+++ components/Main.js
@@ -5,6 +5,16 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchUser } from '../redux/actions/index' 
 
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import FeedScreen from './main/Feed'
+import ProfileScreen from './main/Profile'
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+const EmptyScreen = ()=> {return(null)}
+
+const Tab = createMaterialBottomTabNavigator();
 
 export class Main extends Component {
     componentDidMount(){
@@ -12,13 +22,37 @@ export class Main extends Component {
     }
 
     render() {
-        const { currentUser } = this.props;
-        console.log(currentUser)
-        if(currentUser == undefined){ return(<View/>) }
         return (
-            <View style={{ flex:1, justifyContent:'center'}}>
-				<Text>{currentUser} is logged in</Text>
-			</View>
+            <Tab.Navigator initialRouteName="Feed" labeled={false} >
+                <Tab.Screen name="Feed" component={FeedScreen} 
+                    options = {{
+                        tabBarIcon: ({ color, size })=> (
+                            <MaterialCommunityIcons name="home" color={color} size={26}/>
+                        )
+                    }}
+                />
+                <Tab.Screen name="AddContainer" component={EmptyScreen} 
+                    listeners={ ({ navigation })=> ({
+                            tabPress: event=> {
+                                event.preventDefault()
+                                navigation.navigate("Add")
+                            }
+                        }) 
+                    }
+                    options = {{
+                        tabBarIcon: ({ color, size })=> (
+                            <MaterialCommunityIcons name="plus-box" color={color} size={26}/>
+                        )
+                    }}
+                />
+                <Tab.Screen name="Profile" component={ProfileScreen} 
+                    options = {{
+                        tabBarIcon: ({ color, size })=> (
+                            <MaterialCommunityIcons name="account-circle" color={color} size={26}/>
+                        )
+                    }}
+                />
+            </Tab.Navigator>
         )
     }
 }
