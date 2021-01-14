e42f967c03ad800f01fc5db89303555fd418af6e
Abdulla <abdulla.binissa.176@gmail.com>
2021-01-13 21:32:06 -0800

    Use Camera/Gallery
    https://docs.expo.io/versions/latest/sdk/imagepicker/
    https://docs.expo.io/versions/latest/sdk/camera/
    Added functionality to 'Add' tab
     - Able to take picture (with front or back camera)
     - Able to choose photo from gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,41 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
 
-export default function ImagePickerExample() {
+export default function App() {
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
     const [image, setImage] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
 
-    useEffect(() => {
-        (async () => {
-        if (Platform.OS !== 'web') {
-            const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-            if (status !== 'granted') {
-                alert('Sorry, we need camera roll permissions to make this work!');
-            }
-        }
+    useEffect( ()=> {
+        (async ()=> {
+            const cameraStatus = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus.status === 'granted');
+
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
         })();
     }, []);
 
+    const takePicture = async ()=> {
+        if(camera){
+            const data = await camera.takePictureAsync(null)
+            setImage(data.uri)
+        }
+    }
+
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
-            aspect: [4, 3],
+            aspect: [1, 1],
             quality: 1,
         });
-
         console.log(result);
-
+    
         if (!result.cancelled) {
             setImage(result.uri);
         }
-    };
+      };
 
+    if (hasGalleryPermission === null || hasCameraPermission === null) {
+        return <View />;
+    }
+    if (hasGalleryPermission === false || hasCameraPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Button title="Pick an image from camera roll" onPress={pickImage} />
-            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <View style={styles.container}>
+            <View style={styles.cameraContainer}>
+                <Camera 
+                    ref={ ref => setCamera(ref) }
+                    style={styles.fixedRatio} 
+                    type={type}
+                    ratio={'1:1'}
+                />
+            </View>
+            <Button
+                title="Flip Image"
+                onPress={ ()=> {
+                setType(
+                    type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+                }}>
+            </Button>
+            <Button title="Take Picture" onPress={ ()=> takePicture() }/>
+            <Button title="Pick Image from Gallery" onPress={ ()=> pickImage() }/>
+            { image && <Image source={{ uri:image }} styles={{ flex:1 }} /> }
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio: {
+        flex:1,
+        aspectRatio: 1
+    }
+})
