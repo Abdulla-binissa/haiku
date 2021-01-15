2172e922f7dbf88e40c3b415022475a81efcff96
Abdulla <abdulla.binissa.176@gmail.com>
2021-01-14 16:58:13 -0800

    Save image post on firestore/firebase storage

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+export default function Add({navigation}) {
     const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [hasCameraPermission, setHasCameraPermission] = useState(null);
     const [camera, setCamera] = useState(null);
@@ -69,7 +70,8 @@ export default function App() {
             </Button>
             <Button title="Take Picture" onPress={ ()=> takePicture() }/>
             <Button title="Pick Image from Gallery" onPress={ ()=> pickImage() }/>
-            { image && <Image source={{ uri:image }} styles={{ flex:1 }} /> }
+            <Button title="Save" onPress={ ()=> navigation.navigate('Save', {image}) }/>
+            { image && <Image source={{ uri:image }} style={{ flex:1 }} /> }
         </View>
     );
 }
